f936b24ff5e57717dc208c6caf7010b892575345
Tomas Maly <malytomas@ucpu.cz>
2023-04-14 01:01:39 +0200

    update engine (gui builder)

--- sources/gui.cpp
+++ sources/gui.cpp
@@ -1,86 +1,27 @@
 #include "common.h"
 
-#include <cage-core/entities.h>
-#include <cage-engine/scene.h>
-#include <cage-engine/guiComponents.h>
+#include <cage-engine/guiBuilder.h>
 #include <cage-simple/engine.h>
 
 namespace
 {
-	uint32 playerPositionLabel;
-	uint32 terrainGenerationProgressLabel;
-
 	void engineUpdate()
 	{
 		EntityManager *ents = engineGuiEntities();
-
-		{ // player position
-			GuiTextComponent &t = ents->get(playerPositionLabel)->value<GuiTextComponent>();
-			t.value = Stringizer() + playerPosition;
-		}
-
-		{ // terrain generation progress
-			GuiTextComponent &t = ents->get(terrainGenerationProgressLabel)->value<GuiTextComponent>();
-			t.value = Stringizer() + terrainGenerationProgress * 100 + " %";
-		}
+		ents->get(1)->value<GuiTextComponent>().value = Stringizer() + playerPosition;
+		ents->get(2)->value<GuiTextComponent>().value = Stringizer() + terrainGenerationProgress * 100 + " %";
 	}
 
 	void engineInitialize()
 	{
-		EntityManager *ents = engineGuiEntities();
-
-		Entity *panel = nullptr;
-		{ // panel
-			Entity *wrapper = ents->createUnique();
-			GuiScrollbarsComponent &sc = wrapper->value<GuiScrollbarsComponent>();
-			panel = ents->createUnique();
-			GuiParentComponent &parent = panel->value<GuiParentComponent>();
-			parent.parent = wrapper->name();
-			GuiPanelComponent &g = panel->value<GuiPanelComponent>();
-			GuiLayoutTableComponent &lt = panel->value<GuiLayoutTableComponent>();
-		}
-
-		{ // player position
-			{ // label
-				Entity *e = ents->createUnique();
-				GuiParentComponent &p = e->value<GuiParentComponent>();
-				p.parent = panel->name();
-				p.order = 1;
-				GuiLabelComponent &l = e->value<GuiLabelComponent>();
-				GuiTextComponent &t = e->value<GuiTextComponent>();
-				t.value = "Position: ";
-			}
-			{ // value
-				Entity *e = ents->createUnique();
-				GuiParentComponent &p = e->value<GuiParentComponent>();
-				p.parent = panel->name();
-				p.order = 2;
-				GuiLabelComponent &l = e->value<GuiLabelComponent>();
-				GuiTextComponent &t = e->value<GuiTextComponent>();
-				playerPositionLabel = e->name();
-			}
-		}
-
-		{ // terrain generation progress
-			{ // label
-				Entity *e = ents->createUnique();
-				GuiParentComponent &p = e->value<GuiParentComponent>();
-				p.parent = panel->name();
-				p.order = 3;
-				GuiLabelComponent &l = e->value<GuiLabelComponent>();
-				GuiTextComponent &t = e->value<GuiTextComponent>();
-				t.value = "Loading: ";
-			}
-			{ // value
-				Entity *e = ents->createUnique();
-				GuiParentComponent &p = e->value<GuiParentComponent>();
-				p.parent = panel->name();
-				p.order = 4;
-				GuiLabelComponent &l = e->value<GuiLabelComponent>();
-				GuiTextComponent &t = e->value<GuiTextComponent>();
-				terrainGenerationProgressLabel = e->name();
-			}
-		}
+		Holder<GuiBuilder> g = newGuiBuilder(engineGuiEntities());
+		auto _1 = g->alignment(Vec2(0));
+		auto _2 = g->panel();
+		auto _3 = g->verticalTable(2);
+		g->label().text("Position: ");
+		g->setNextName(1).label().text("");
+		g->label().text("Loading: ");
+		g->setNextName(2).label().text("");
 	}
 
 	class Callbacks
